6023cc342e19e57300723594d608b59ca7dbc246
rldnrl <devpark1002@gmail.com>
2022-06-18 19:19:19 +0900

    feat: ModelDuck 구현

---
+++ java/Chap01/src/ModelDuck.java
@@ -0,0 +1,10 @@
+public class ModelDuck extends Duck {
+    public ModelDuck() {
+        flyBehavior = new FlyNoWay();
+        quackBehavior = new Quack();
+    }
+
+    public void display() {
+        System.out.println("저는 모형 오리입니다.");
+    }
+}
