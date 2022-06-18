f9094aa4261f5db67ebb60f5aff32e5c79589cfa
rldnrl <devpark1002@gmail.com>
2022-06-18 18:33:33 +0900

    feat: Duck 추상 클래스

---
+++ java/Chap01/src/Duck.java
@@ -0,0 +1,19 @@
+public abstract class Duck {
+  FlyBehavior flyBehavior;
+  QuackBehavior quackBehavior;
+
+  public Duck() {}
+
+  public abstract void display();
+  public void performFly() {
+    flyBehavior.fly();
+  }
+
+  public void performQuack() {
+    quackBehavior.quack();
+  }
+
+  public void swim() {
+    System.out.println("모든 오리는 물에 뜹니다. 가짜 오리도 뜨죠.");
+  }
+}
