a6eeb18ac92f3ab3abf8a5d3b890c2cd97125ffc
rldnrl <devpark1002@gmail.com>
2022-06-20 19:35:21 +0900

    feat: indent 수정

--- java/Chap01/src/Duck.java
+++ java/Chap01/src/Duck.java
@@ -1,27 +1,27 @@
 public abstract class Duck {
-  FlyBehavior flyBehavior;
-  QuackBehavior quackBehavior;
+    FlyBehavior flyBehavior;
+    QuackBehavior quackBehavior;
 
-  public Duck() {}
+    public Duck() {}
 
-  public abstract void display();
-  public void performFly() {
-      flyBehavior.fly();
-  }
+    public abstract void display();
+    public void performFly() {
+        flyBehavior.fly();
+    }
 
-  public void performQuack() {
-      quackBehavior.quack();
-  }
+    public void performQuack() {
+        quackBehavior.quack();
+    }
 
-  public void swim() {
-      System.out.println("모든 오리는 물에 뜹니다. 가짜 오리도 뜨죠.");
-  }
+    public void swim() {
+        System.out.println("모든 오리는 물에 뜹니다. 가짜 오리도 뜨죠.");
+    }
 
-  public void setFlyBehavior(FlyBehavior fb) {
-    flyBehavior = fb;
-  }
+    public void setFlyBehavior(FlyBehavior fb) {
+      flyBehavior = fb;
+    }
 
-  public void setQuackBehavior(QuackBehavior qb) {
-    quackBehavior = qb;
-  }
+    public void setQuackBehavior(QuackBehavior qb) {
+      quackBehavior = qb;
+    }
 }
