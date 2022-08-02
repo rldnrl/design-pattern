c03c891ca6ceb5db0ca60379f1bf7055ec4206fa
rldnrl <devpark1002@gmail.com>
2022-08-02 11:35:48 +0900

    feat: beverage 추상 클래스

---
+++ java/Chap03/src/Beverage.java
@@ -0,0 +1,9 @@
+public abstract class Beverage {
+    String description = "제목 없음";
+
+    public String getDescription() {
+        return description;
+    }
+
+    public abstract double cost();
+}
